f60a5d88c7419ce24e72ed1a07f9464c386a3e35
lasse.alfastsen@gmail.com <Yx1zepn45@BJnbO&a5VX#OdwDX5gnDDKJE^%b!I>
2021-11-12 21:55:28 +0100

    .

--- New-FolderStructure.ps1
+++ New-FolderStructure.ps1
@@ -4,13 +4,13 @@ if (-not (Test-Path ".\$Year")) {
     New-Item -ItemType Directory -Name $Year -Path ".\"
     New-Item -ItemType File -Name "Results $Year.md" -Path ".\$Year"
     Add-Content -LiteralPath .\README.md -Value "[$Year Results](2021/Results%20$Year.md)"
-    Add-Content -LiteralPath ".\$Year\Results $Year.md" -Value "# This is how it is going :-)`n|Day|Part 1|Part 2|`n|--|--|--|"
+    Add-Content -LiteralPath ".\$Year\Results $Year.md" -Value "# This is how it is going :-)`n|Day|Part 1|Part 2|`n|--|:--:|:--:|"
 }
 
 for ($i = 1; $i -le 25; $i++) {
     New-Item -ItemType Directory -Name "Day $i" -Path ".\$Year"
     New-Item  -ItemType File -Name "Day $i.ps1" -Path ".\$Year\Day $i"
-    Add-Content -LiteralPath ".\$Year\Results $Year.md" -Value "|Day $i| | |"
+    Add-Content -LiteralPath ".\$Year\Results $Year.md" -Value "|Day $i| :heavy_minus_sign: | :heavy_minus_sign: |"
 }
 
 
