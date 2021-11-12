8dd975a50794c1ec53f7aeef7211869d863e9437
lasse.alfastsen@gmail.com <Yx1zepn45@BJnbO&a5VX#OdwDX5gnDDKJE^%b!I>
2021-11-12 21:36:33 +0100

    .

--- New-FolderStructure.ps1
+++ New-FolderStructure.ps1
@@ -2,9 +2,16 @@ $Year = (Get-Date).ToString('yyyy')
 
 if (-not (Test-Path ".\$Year")) {
     New-Item -ItemType Directory -Name $Year -Path ".\"
+    New-Item -ItemType File -Name "Results $Year.md" -Path ".\$Year"
+    Add-Content -LiteralPath ".\$Year\Results $Year.md" -Value "|Day|Part 1|Part 2|
+    |--|--|--|"
 }
 
 for ($i = 1; $i -le 25; $i++) {
     New-Item -ItemType Directory -Name "Day $i" -Path ".\$Year"
     New-Item  -ItemType File -Name "Day $i.ps1" -Path ".\$Year\Day $i"
+    Add-Content -LiteralPath ".\$Year\Results $Year.md" -Value "|Day $i| | |"
 }
+
+
+New-Item -ItemType Directory -Name "Test" | New-Item -ItemType File -Name "Hello.txt"
