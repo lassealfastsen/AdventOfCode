20065109473640660dbb07d7a51ce63a8b7a3ee6
lasse.alfastsen@gmail.com <Yx1zepn45@BJnbO&a5VX#OdwDX5gnDDKJE^%b!I>
2021-11-12 21:40:00 +0100

    .

--- New-FolderStructure.ps1
+++ New-FolderStructure.ps1
@@ -3,8 +3,7 @@ $Year = (Get-Date).ToString('yyyy')
 if (-not (Test-Path ".\$Year")) {
     New-Item -ItemType Directory -Name $Year -Path ".\"
     New-Item -ItemType File -Name "Results $Year.md" -Path ".\$Year"
-    Add-Content -LiteralPath ".\$Year\Results $Year.md" -Value "|Day|Part 1|Part 2|
-    |--|--|--|"
+    Add-Content -LiteralPath ".\$Year\Results $Year.md" -Value "|Day|Part 1|Part 2|`n|--|--|--|"
 }
 
 for ($i = 1; $i -le 25; $i++) {
