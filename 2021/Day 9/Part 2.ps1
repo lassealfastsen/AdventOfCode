7366412ebce76be34b22787f1e29e1108b1f7306
lasse.alfastsen@gmail.com <Yx1zepn45@BJnbO&a5VX#OdwDX5gnDDKJE^%b!I>
2021-12-11 00:22:06 +0100

    Day 9 Part 2 Optimized

--- 2021/Day 9/Part 2.ps1
+++ 2021/Day 9/Part 2.ps1
@@ -17,41 +17,28 @@ foreach ($row in $inputContent) {
 
 $lowpoints = @()
 
-for ($y = 0; $y -lt $grid.GetLength(0); $y++) {
-    for ($x = 0; $x -lt $grid.GetLength(1); $x++) {
-        $l = 9
-        $r = 9
-        $u = 9
-        $d = 9
+$gridsize = $grid.GetLength(0)
 
-        if ($x -gt 0) {
-            $l = $grid[$y, ($x-1)]
-        }
-        if ($x -lt ($grid.GetLength(1)-1)) {
-            $r = $grid[$y, ($x+1)]
-        }
-        if ($y -gt 0) {
-            $u = $grid[($y-1),$x]
-        }
-        if ($y -lt ($grid.GetLength(0)-1)){
-            $d = $grid[($y+1),$x]
-        }
+for ($y = 0; $y -lt $gridsize; $y++) {
+    for ($x = 0; $x -lt $gridsize; $x++) {
 
-        if ($grid[$y,$x] -lt (@($u, $d, $l, $r) | Sort | Select -First 1)) {
-           # Write-Host $grid[$y,$x] -ForegroundColor Yellow -NoNewline
+        $val = $grid[$y,$x]
+
+        if (
+            (($x -eq 0) -or ($val -lt $grid[$y,($x-1)])) `
+            -and (($x -eq $gridsize) -or ($val -lt $grid[$y,($x+1)])) `
+            -and (($y -eq 0) -or ($val -lt $grid[($y-1),$x])) `
+            -and (($y -eq $gridsize) -or ($val -lt $grid[($y+1),$x]))
+        ) {
             $lowpoints += "$x,$y"
-        } else {
-            #Write-Host $grid[$y,$x] -NoNewline
         }
     }
-    #Write-Host
 }
 
 Function Get-BasinSize {
     Param(
         [Parameter(Mandatory=$true, Position=1)][string]$startPoint
     )
-
     
     $queue = @()
     $searched = @()
@@ -61,60 +48,44 @@ Function Get-BasinSize {
     while ($queue.count -gt 0) {
     
 
+
         $x = [int]$queue[0].Split(',')[0]
         $y = [int]$queue[0].Split(',')[1]
-    
-        if ($grid[$y,$x] -lt 9) {
-            $basinsize++
-            #"$x,$y"
-        }
+   
+        
+        $basinsize++
+        
         if ($queue.Count -eq 1) {$queue = @()} else {$queue = $queue[1..($queue.Length-1)]}
         $searched += "$x,$y"
     
-        $l = 9
-        $r = 9
-        $u = 9
-        $d = 9
+
+        if (
+            (($x -gt 0) -and ("$($x-1),$y" -notin $searched) -and ("$($x-1),$y" -notin $queue) -and ($grid[$y,($x-1)] -ne 9))
+        ) { $queue += "$($x-1),$y" }
     
-    
-        if ($x -gt 0) {
-            $l = $grid[$y, ($x-1)]
-        }
-        if ($x -lt ($grid.GetLength(1)-1)) {
-            $r = $grid[$y,($x+1)]
-        }
-        if ($y -gt 0) {
-            $u = $grid[($y-1), $x]
-        }
-        if ($y -lt ($grid.GetLength(0)-1)) {
-            $d = $grid[($y+1),$x]
-        }
-    
-        if (($l -ne 9) -and ("$($x-1),$y" -notin $searched) -and ("$($x-1),$y" -notin $queue)) {
-            $queue += "$($x-1),$y"
-        }
-        if (($r -ne 9) -and ("$($x+1),$y" -notin $searched) -and ("$($x+1),$y" -notin $queue)) {
-            $queue += "$($x+1),$y"
-        }
-        if (($u -ne 9) -and ("$x,$($y-1)" -notin $searched) -and ("$x,$($y-1)" -notin $queue)) {
-            $queue += "$x,$($y-1)"
-        }
-        if (($d -ne 9) -and ("$x,$($y+1)" -notin $searched) -and ("$x,$($y+1)" -notin $queue)) {
-            $queue += "$x,$($y+1)"
-        }
+        if (
+            (($x -lt $gridsize-1) -and ("$($x+1),$y" -notin $searched) -and ("$($x+1),$y" -notin $queue) -and ($grid[$y,($x+1)] -ne 9))
+        ) { $queue += "$($x+1),$y" }
+
+        if (
+            (($y -gt 0) -and ("$x,$($y-1)" -notin $searched) -and ("$x,$($y-1)" -notin $queue) -and ($grid[($y-1),$x] -ne 9))
+        ) { $queue += "$x,$($y-1)" }
+        
+        if (
+            (($y -lt $gridsize-1) -and ("$x,$($y+1)" -notin $searched) -and ("$x,$($y+1)" -notin $queue) -and ($grid[($y+1),$x] -ne 9))
+        ) { $queue += "$x,$($y+1)" }
     }
 
     return $basinsize
 
 }
 
+#Get-BasinSize $lowpoints[0]
 
-$basins = @()
-
-foreach ($lowpoint in $lowpoints) {
-    $basins += Get-BasinSize $lowpoint
-}
-
-$answer = 1
-$basins | Sort-Object -Descending | Select-Object -First 3 | % { $answer = ($answer * $_) }
-$answer
+ $basins = @()
+ foreach ($lowpoint in $lowpoints) {
+     $basins += Get-BasinSize $lowpoint
+ }
+ $answer = 1
+ $basins | Sort-Object -Descending | Select-Object -First 3 | ForEach-Object { $answer = ($answer * $_) }
+ $answer
