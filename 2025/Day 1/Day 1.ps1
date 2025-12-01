897a363812c0e511c84d756f7ce4270d3387651f
lasse alfastsen <10941370+lassealfastsen@users.noreply.github.com>
2025-12-01 16:47:25 +0100

    Day 1 optimized

--- 2025/Day 1/Day 1.ps1
+++ 2025/Day 1/Day 1.ps1
@@ -1,7 +1,9 @@
-$reader = New-Object System.IO.StreamReader('.\AdventOfCode\2025\Day 1\input.txt')
+$reader = New-Object System.IO.StreamReader('C:\Temp\AoC\AdventOfCode\2025\Day 1\input.txt')
 
 
 $counter = 0
+$counter1 = 0
+$wholerotations = 0 
 #$counter2 = 0
 $pos = 50
 
@@ -9,70 +11,33 @@ while ($null -ne ($read = $reader.ReadLine())) {
     $regex = ([regex]::Match($read, "([LR])([0-9].*)"))
     $dir = $regex.Groups[1].Value
     $amount = [int]$regex.Groups[2].Value
+    
+    $wholerotations += [Math]::Floor($amount / 100)
+    $amount %= 100
+
     switch ($dir) {
         'L' {
-            $pos -= $amount
+            if ($pos -ne 0 -and $amount -gt $pos) {
+                $counter++
+            }
+
+            $pos = (100 + ($pos - $amount)) % 100
         }
         'R' {
-            $pos += $amount
+            if ($amount -gt (100 - $pos)) {
+                $counter++
+            }
+            $pos = ($pos + $amount) % 100
         }
     }
-
-    if ($pos -gt 100) {
-        $pos = $pos % 100
+    if ($pos -in @(0, 100)) {
+        $counter1++
+        $counter++
     }
-    elseif ($pos -lt 0) {
-        $pos = 100 - ([Math]::Abs($pos) % 100)
-    } if ($pos -eq 0 -or $pos -eq 100) { $counter++ }
 
 }
-
-Write-Host "Part 1: $counter" -ForegroundColor Green
-
-$reader.Close()
-$reader.Dispose()
-
-
-
-#Brute forcing part 2 cause i am lazy
-
-$reader = New-Object System.IO.StreamReader('.\AdventOfCode\2025\Day 1\input.txt')
-
-
-$counter = 0
-#$counter2 = 0
-$pos = 50
-
-while ($null -ne ($read = $reader.ReadLine())) {
-    $regex = ([regex]::Match($read, "([LR])([0-9].*)"))
-    $dir = $regex.Groups[1].Value
-    $amount = [int]$regex.Groups[2].Value
-    switch ($dir) {
-        'L' {
-            $mod = -1
-        }
-        'R' {
-            $mod = 1
-        }
-    }
-
-    while ($amount -gt 0) {
-        $pos += $mod
-        if ($pos -eq 100) {
-            $pos = 0
-            $counter++
-        }
-        elseif ($pos -eq 0) {
-            $counter++
-        }
-        elseif ($pos -eq -1) {
-            $pos = 99
-        }
-        $amount--
-    }
-}
-
-Write-Host "Part 2: $counter" -ForegroundColor Green
+Write-Host "Part 1: $counter1" -ForegroundColor Green
+Write-Host "Part 2: $counter + $wholerotations = $($counter+$wholerotations)" -ForegroundColor Green
 
 $reader.Close()
 $reader.Dispose()
